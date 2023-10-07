563405d3eaf846a13f97d631e123cc415b72dd78
TurtlePower <agnkvdm@gmail.com>
2023-10-07 19:34:22 +0300

    fix favorite icon sizes

--- src/components/IconBtn/IconBtn.styled.jsx
+++ src/components/IconBtn/IconBtn.styled.jsx
@@ -7,8 +7,8 @@ export const IconButton = styled.button`
 `;
 
 export const StyledSvg = styled.svg`
-  width: 20px;
-  height: 20px;
+  width: 18px;
+  height: 18px;
 
   stroke: ${props => (props.favorite ? '#3470FF' : 'rgba(255, 255, 255, 0.8)')};
   fill: ${props => (props.favorite ? '#3470FF' : 'none')};
